b328318e6799e9428754f83fb78bf11e2c68a5ff
samtalks <sam.ikiru@gmail.com>
2013-10-04 17:20:54 -0400

    update

--- 10-04 scrape lab/insert_code.rb
+++ 10-04 scrape lab/insert_code.rb
@@ -27,6 +27,14 @@ db.execute(
 ###########  CREATE TABLE END
 ###########  INSERT START
 
+index_ = {
+  
+}
+
+
+profiles = {
+
+}
 
 
 db.execute( 
@@ -42,6 +50,20 @@ db.execute(
 #   intro TEXT,
 #   photo_url TEXT
 # );git
+
+CREATE TABLE student_profile (
+  id INTEGER PRIMARY KEY,
+  name_id INTEGER REFERENCES index,
+  thumbnail_url TEXT,
+  background_url TEXT,
+  biography TEXT,
+  education TEXT,
+  work TEXT,
+  github TEXT,
+  treehouse TEXT,
+  codeschool TEXT,
+  coderwall TEXT,
+);
 
 
 ###########  INSERT END
