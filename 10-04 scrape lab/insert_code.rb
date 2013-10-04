909dc1dca87e5c1bd962c72e539a4a68432b2bb9
samtalks <sam.ikiru@gmail.com>
2013-10-04 18:18:57 -0400

    merging work in insert_code.rb

--- 10-04 scrape lab/insert_code.rb
+++ 10-04 scrape lab/insert_code.rb
@@ -26,46 +26,48 @@ db.execute(
 =end
 ###########  CREATE TABLE END
 ###########  INSERT START
-=begin
 
 
-index_ = {
-  
+index_hash = {
+  "name" => ["Sam", "Jeanne"],
+  "tagline" => ["I'm Sam", "I'm Jeanne"],
+  "blurb" => ["stuff about me I'm Sam", "I'm Jeanna and I like coding"],
+  "photo_url" => ["www.photo.com", "www.photo.com"]
 }
 
-
-profiles = {
-
-}
-
-
-db.execute( 
-  "CREATE INSERT #{index_hash} (
-    #{key_int_primary} PRIMARY KEY, 
-    #{key_int} INTEGER,
-    #{key_text} TEXT);")
+index.each do |key, value|
+  index[key].each do |item|
+    db.execute( 
+      "INSERT INTO index(name, tagline, blurb, photo_url) VALUES(#{item})"
+  end
+end
 
 # CREATE TABLE index (
 #   id INTEGER PRIMARY KEY,
 #   name TEXT,
 #   tagline TEXT,
-#   intro TEXT,
+#   blurb TEXT,
 #   photo_url TEXT
-# );git
+# );
 
-CREATE TABLE student_profile (
-  id INTEGER PRIMARY KEY,
-  name_id INTEGER REFERENCES index,
-  thumbnail_url TEXT,
-  background_url TEXT,
-  biography TEXT,
-  education TEXT,
-  work TEXT,
-  github TEXT,
-  treehouse TEXT,
-  codeschool TEXT,
-  coderwall TEXT,
-);
+# CREATE TABLE student_profile (
+#   id INTEGER PRIMARY KEY,
+#   name_id INTEGER REFERENCES index,
+#   thumbnail_url TEXT,
+#   background_url TEXT,
+#   biography TEXT,
+#   education TEXT,
+#   work TEXT,
+#   github TEXT,
+#   treehouse TEXT,
+#   codeschool TEXT,
+#   coderwall TEXT,
+# );
 
-=end
+# db.execute( "SELECT...." ) do |row|
+#   p row
+# end
+
+
+
 ###########  INSERT END
